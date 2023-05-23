550ef5053224dd1ec441b2dac2d23661ce12eda0
Jay Lim <jlimoman@gmail.com>
2023-05-23 00:20:30 -0600

    complete python-p3-comprehensions-and-generators lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
-
+    even_num_list = [num for num in num_list if num % 2 == 0]
+    return even_num_list
+    
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return []
+    exclamation_list = [sentence + "!" for sentence in sentence_list  ]
+    return exclamation_list
